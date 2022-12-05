eecc2b182e14014fac235aee44cdcfadda6d093c
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-12-05 13:36:26 -0700

    Update ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //references the Product Models ID
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //references the Tag Models ID
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
